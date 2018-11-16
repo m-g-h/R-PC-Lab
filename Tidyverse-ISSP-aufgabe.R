b6b46c370ecdfd7a8b03492070e08ee579780d8d
thiesben <b.thies@zeppelin-university.net>
2018-11-16 23:49:08 +0100

    add tidyverse aufgabe and finalised tidyverse presentation + data

--- Tidyverse-ISSP-aufgabe.R
+++ Tidyverse-ISSP-aufgabe.R
@@ -1,4 +1,42 @@
 library(tidyverse)
+library(nycflights13)
+
+flights %>% 
+  filter(month <= 03 & distance > 2500)
+
+flights %>% 
+  arrange(day, desc(dep_delay))
+
+flights %>% 
+  select(carrier, tail_number = tailnum)
+
+flights %>% 
+  mutate(id = 1:n(), 
+         dist_km = distance * 1.60934) %>% 
+  select(id, distance, dist_km)
+
+flights %>% 
+  filter(!is.na(arr_delay))
+  summarise(
+    min_delay = min(arr_delay),
+    avg_delay = mean(arr_delay),
+    max_delay = max(arr_delay)
+  )
+
+flights %>% 
+  filter(!is.na(arr_delay)) %>% 
+  group_by(carrier) %>% 
+  mutate(delta_arr_delay = arr_delay - mean(arr_delay)) %>% 
+  select(carrier, delta_arr_delay)
+
+flights %>% 
+  filter(!is.na(arr_delay)) %>% 
+  group_by(carrier) %>% 
+  summarise(
+    mean_delay = mean(arr_delay),
+    median_delay = median(arr_delay)
+  )
+
 library(haven)
 
 ISSP <- read_dta("written/data/ISSP.dta")
@@ -16,5 +54,4 @@ Germany %>%
   group_by(sex) %>% 
   summarise(percHighInc=mean(highInc, na.rm=T)) %>% 
   arrange(percHighInc)
-  
   
